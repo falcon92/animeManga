fe1aeeca213e0083a60cd09505e6283e0fbd5da6
SuperGogeta <manga@manga.com>
2018-09-22 13:22:56 -0500

    animeManga primer commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Producto(models.Model):
+    autor = models.CharField(max_length=50)
+    sinopsis = models.TextField()
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateTimeField(blank=True, null=True)
+    genero = models.CharField(max_length=50)
+    capitulos = models.IntegerField()
+    image = models.ImageField(upload_to='img/', blank=True)
+
+
+    def publicar(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
