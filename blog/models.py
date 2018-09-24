b2db997b64f8d38d01a52593492f791ac81e29bb
SuperGogeta <manga@manga.com>
2018-09-24 11:52:51 -0500

    modifique el modelo

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,15 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=50, null=True)
     autor = models.CharField(max_length=50)
-    sinopsis = models.TextField()
+    sinopsis = models.CharField(max_length=364)
+    descripcion = models.TextField()
     fecha_inicio = models.DateTimeField()
     fecha_fin = models.DateTimeField(blank=True, null=True)
     genero = models.CharField(max_length=50)
     capitulos = models.IntegerField()
     image = models.ImageField(upload_to='img/', blank=True)
+    imagen_portada = models.ImageField(upload_to='img/', blan=True)
+    fecha_publicacion = models.DateTimeField.now()
 
 
     def publicar(self):
