969f5d70ce1593570b12202a3dfe9e5cf64fa7b8
SuperGogeta <manga@manga.com>
2018-09-24 12:09:43 -0500

    errores menores

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,14 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=50, null=True)
     autor = models.CharField(max_length=50)
     sinopsis = models.CharField(max_length=364)
-    descripcion = models.TextField()
+    descripcion = models.TextField(blank=True)
     fecha_inicio = models.DateTimeField()
     fecha_fin = models.DateTimeField(blank=True, null=True)
     genero = models.CharField(max_length=50)
     capitulos = models.IntegerField()
     image = models.ImageField(upload_to='img/', blank=True)
-    imagen_portada = models.ImageField(upload_to='img/', blan=True)
-    fecha_publicacion = models.DateTimeField.now()
+    imagen_portada = models.ImageField(upload_to='img/', blank=True)
+    fecha_publicacion = models.DateTimeField(blank=True, null=True)
 
 
     def publicar(self):
